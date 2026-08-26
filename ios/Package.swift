49e3eb3fafcb73c760df3a54bfe4ce3701253724
yyyug <nyyueng@gmail.com>
2026-08-26 10:48:54 +0800

    Remove llama.cpp dependency (placeholder doesn't need it yet)

--- ios/Package.swift
+++ ios/Package.swift
@@ -8,14 +8,12 @@ let package = Package(
         .macOS(.v14)
     ],
     dependencies: [
-        .package(url: "https://github.com/ggerganov/llama.cpp.git", branch: "master"),
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
     ],
     targets: [
         .target(
             name: "VoxyzCore",
             dependencies: [
-                .product(name: "llama", package: "llama.cpp"),
                 .product(name: "GRDB", package: "GRDB.swift"),
             ],
             path: "Voxyz",
